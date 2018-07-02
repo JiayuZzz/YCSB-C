9b6c38d4595f7c55c1394c61724e988989ddce1f
yuwolf <wujy.cs@gmail.com>
2018-07-02 21:54:04 +0800

    run leveldb python script

---
+++ run_leveldb.py
@@ -0,0 +1,24 @@
+import tools
+import sys
+
+phase = sys.argv[1]
+dbfilename = "./test"
+workload = "./workloads/workloada.spec"
+
+configs = {
+    "bloomBits":"4",
+    "seekCompaction":"true",
+    "directIO":"false",
+    "compression":"false"
+}
+
+#set configs
+if __name__ == '__main__':
+    for cfg in configs:
+        tools.modifyConfig("./configDir/leveldb_config.ini","config",cfg,configs[cfg])
+
+    if phase=="load": 
+        tools.load(dbfilename,workload)
+
+    if phase=="run":
+        tools.run(dbfilename,workload)
---
+++ tools.py
@@ -0,0 +1,26 @@
+import configparser
+import os
+
+# repair case problem in configparser package
+class MyConfigParser(configparser.ConfigParser):
+    def __init__(self, defaults=None):
+        configparser.ConfigParser.__init__(self, defaults=defaults)
+    def optionxform(self, optionstr):
+        return optionstr
+
+
+def modifyConfig(file,field,key,value):
+    parser = MyConfigParser()
+    parser.read(file)
+    parser.set(field,key,value)
+    parser.write(open(file,'w'))
+
+
+def load(dbfilename,workload,resultfile="-1"):
+    if resultfile=="-1":
+        os.system("./ycsbc -db leveldb -dbfilename {0} -threads 1 -P {1} -skipLoad false".format(dbfilename,workload))
+    else:
+        os.system("./ycsbc -db leveldb -dbfilename {0} -threads 1 -P {1} -skipLoad false > {2}".format(dbfilename,workload,resultfile))
+
+def run(dbfilename,workload,resultfile="-1"):
+    os.system("./ycsbc -db leveldb -dbfilename {0} -threads 1 -P {1} -skipLoad true > {2}".format(dbfilename,workload,resultfile))
