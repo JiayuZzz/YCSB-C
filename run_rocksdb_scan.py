fb910e88bb3ccd48386e7efd72fdf85a724185f2
yuwolf <wujy.cs@gmail.com>
2019-11-12 13:04:44 +0000

    rocksdb core

---
+++ run_rocksdb_scan.py
@@ -0,0 +1,53 @@
+import funcs
+import sys
+import os
+
+dbPath = "/mnt/rocksdb/"
+#dbPath = "/mnt/raidstore/"
+valueSizes = ["1KB","4KB","8KB"]
+dbSize = "100GB"
+for valueSize in valueSizes:
+    dbfilename = dbPath+"rocksdb"+valueSize+dbSize
+    workload = "./workloads/workload"+valueSize+dbSize+".spec"
+    memtable = 64
+    resultfile = "./resultDir/rocksdb"+valueSize+dbSize+"memtable"+str(memtable)
+
+
+    configs = {
+        "bloomBits":"10",
+        "seekCompaction":"false",
+        "directIO":"false",
+        "noCompaction":"true",
+        "compression":"false",
+        "blockCache":str(6*1024*1024),
+        "memtable":str(memtable*1024*1024),
+        "numThreads":str(8),
+        "tiered":"false"
+    }
+
+    phase = sys.argv[1]
+
+    if __name__ == '__main__':
+        #set configs
+        os.system("sync && echo 3 > /proc/sys/vm/drop_caches")
+        if phase=="load":
+            configs["noCompaction"] = "false"
+
+        for cfg in configs:
+            funcs.modifyConfig("./configDir/leveldb_config.ini","config",cfg,configs[cfg])
+
+        if len(sys.argv) == 3:
+            resultfile = sys.argv[2]
+
+        if phase=="load":
+            resultfile = resultfile+"_load"
+            funcs.load("rocksdb",dbfilename,workload,resultfile)
+
+        if phase=="run":
+            resultfile = resultfile+"_run"
+            print(resultfile)
+            funcs.run("rocksdb",dbfilename,workload,resultfile)
+
+        if phase=="both":
+            resultfile1 = resultfile+"_both"
+            funcs.both("rocksdb",dbfilename,workload,resultfile1)
