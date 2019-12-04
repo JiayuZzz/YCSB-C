fbe91bfe2550dce94b681b581604cb3d44a51e98
wujy-cs <wujy.cs@gmail.com>
2019-12-04 12:27:13 +0000

    nvme scripts

---
+++ run_update_nvme.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+disks = {"vtable":"/dev/nvme0n1p1","titan":"/dev/nvme0n1p1","rocksdb":"/dev/nvme0n1p1","vtablenolarge":"/dev/nvme0n1p1","vtablenomid":"/dev/nvme0n1p1"}
+paths = {"vtable":"/mnt/expdb","titan":"/mnt/titan","rocksdb":"/mnt/rocksdb","vtablenolarge":"/mnt/expdb","vtablenomid":"/mnt/expdb"}
+backupPath = "/mnt/backup/"
+
+for db,disk in disks.items():
+    os.system("umount {0}".format(disk))
+    os.system("mkfs.ext4 {0} -F".format(disk))
+    os.system("mount {0} {1}".format(disk, paths[db]))
+    os.system("python run_{0}.py both".format(db))
